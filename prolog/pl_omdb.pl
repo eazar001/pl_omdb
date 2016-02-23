5e24015e4478ca17fd19f0dcb373adf0c63d0868
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2016-02-23 12:58:05 -0800

    Adding internal search calls.

--- prolog/pl_omdb.pl
+++ prolog/pl_omdb.pl
@@ -2,13 +2,13 @@
 
 :- use_module(library(http/http_open)).
 :- use_module(library(http/json)).
-:- use_module(library(http/http_json)).
+:- use_module(library(uri)).
 :- use_module(library(yall)).
 :- use_module(omdb_types).
 
 
 omdb_api("http://www.omdbapi.com/?t=~s&y=~s&plot=~s&r=json").
-omdb_search("http://www.omdbapi.com/?t=~s&y=~s&plot=~s&r=~s").
+omdb_search("http://www.omdbapi.com/?s=~s&y=~s&page=~s&r=json").
 omdb_poster_api("http://img.omdbapi.com/?apikey=~s&").
 
 
@@ -19,6 +19,22 @@ omdb_dict(Dict, [Title,Year,Plot]) :-
   Params = [T,Y,P],
   maplist(must_be(omdb_param), Params),
   omdb_api(Template),
+  format(string(Request), Template, Params),
+  setup_call_cleanup(
+    (  http_open(Request, Stream, [timeout(20)]),
+       set_stream(Stream, encoding(utf8))
+    ),
+    json_read_dict(Stream, Dict),
+    close(Stream)
+  ).
+
+omdb_search(Dict, [Title,Year,Page]) :-
+  maplist([X]>>ignore(X=""), [Title,Year,Page]),
+  maplist([X,S]>>( uri_encoded(fragment, X, E), atom_string(E, S) ),
+    [Title,Year,Page], [T,Y,P]),
+  Params = [T,Y,P],
+  maplist(must_be(omdb_param), Params),
+  omdb_search(Template),
   format(string(Request), Template, Params),
   setup_call_cleanup(
     (  http_open(Request, Stream, [timeout(20)]),
