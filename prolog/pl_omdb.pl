f4f0f4f74adc0497ab47a6eb6ad4aea202fcd42f
Ebrahim Azarisooreh <ebrahim.azarisooreh@gmail.com>
2017-08-10 18:12:12 -0700

    Formatting.

--- prolog/pl_omdb.pl
+++ prolog/pl_omdb.pl
@@ -1,9 +1,10 @@
-:- module(pl_omdb,
-     [ omdb_fetch/2
-      ,omdb_search/2
-      ,omdb_search_results/3
-      ,omdb_fetch_dict/2
-      ,omdb_search_dict/2 ]).
+:- module(pl_omdb, [
+	 omdb_fetch/2,
+	 omdb_search/2,
+	 omdb_search_results/3,
+	 omdb_fetch_dict/2,
+	 omdb_search_dict/2
+]).
 
 :- use_module(library(http/http_open)).
 :- use_module(library(http/json)).
@@ -35,8 +36,8 @@ omdb_poster_api("http://img.omdbapi.com/?~s&apikey=~s&").
 %  represented by KVPair.
 
 omdb_fetch(Key=Value, Options) :-
-    omdb_call(retrieval, Dict, Options),
-    Value = Dict.Key.
+	omdb_call(retrieval, Dict, Options),
+	Value = Dict.Key.
 
 
 %% omdb_search(?KVPair, +Options) is nondet.
@@ -47,8 +48,8 @@ omdb_fetch(Key=Value, Options) :-
 %  results and the number of results are part of KVPair (Key=Value).
 
 omdb_search(Key=Value, Options) :-
-    omdb_call(search, Dict, Options),
-    Value = Dict.Key.
+	omdb_call(search, Dict, Options),
+	Value = Dict.Key.
 
 
 %% omdb_search_results(?KVPair, +Options, ?NumResults) is nondet.
@@ -58,11 +59,11 @@ omdb_search(Key=Value, Options) :-
 %  the number of search results found by the search query.
 
 omdb_search_results(Key=Value, Options, NumResults) :-
-    omdb_search_dict(Dict, Options),
-    NumResults = Dict.'totalResults',
-    SearchResults = Dict.'Search',
-    member(OneResult, SearchResults),
-    Value = OneResult.Key.
+	omdb_search_dict(Dict, Options),
+	NumResults = Dict.'totalResults',
+	SearchResults = Dict.'Search',
+	member(OneResult, SearchResults),
+	Value = OneResult.Key.
 
 
 %% omdb_fetch_dict(-Dict, +Options) is det.
@@ -71,14 +72,14 @@ omdb_search_results(Key=Value, Options, NumResults) :-
 %  rather than backtracking over individual Key=Value pairs.
 
 omdb_fetch_dict(Dict, Options) :-
-    omdb_call(retrieval, Dict, Options).
+	omdb_call(retrieval, Dict, Options).
 
 
 %% omdb_search_dict(-Dict, +Options) is det.
 %
 %  Like omdb_fetch_dict/2 but for search queries.
 omdb_search_dict(Dict, Options) :-
-    omdb_call(search, Dict, Options).
+	omdb_call(search, Dict, Options).
 
 
 %--------------------------------------------------------------------------------%
@@ -87,51 +88,53 @@ omdb_search_dict(Dict, Options) :-
 
 
 omdb_call(Call, Dict, Options) :-
-    (  Call = retrieval,
-     retrieval_query(Options, Template)
-    ;  Call = search,
-     search_query(Options, Template)
-    ),
-    omdb_api(API),
-    format(string(Request), API, [Template]),
-    omdb_connect(Request, Dict).
+	(	Call = retrieval,
+		retrieval_query(Options, Template)
+	;	Call = search,
+		search_query(Options, Template)
+	),
+	omdb_api(API),
+	format(string(Request), API, [Template]),
+	omdb_connect(Request, Dict).
 
 
 omdb_connect(Request, Dict) :-
-    setup_call_cleanup(
-    (  http_open(Request, Stream, [timeout(20)]),
-       set_stream(Stream, encoding(utf8))
-    ),
-    json_read_dict(Stream, Dict),
-    close(Stream)
-    ).
+	setup_call_cleanup(
+		(	http_open(Request, Stream, [timeout(20)]),
+	   		set_stream(Stream, encoding(utf8))
+		),
+		json_read_dict(Stream, Dict),
+		close(Stream)
+	).
 
 :- begin_tests(pl_omdb).
 
 test(fetch_one_value) :-
-    aggregate_all(
-      count,
-      omdb_fetch('Released'=_Value, [title="Casino Royale",year="2006"]),
-      1
-    ).
+	aggregate_all(
+		count,
+		omdb_fetch('Released'=_Value, [title="Casino Royale",year="2006"]),
+		1
+	).
 
 test(throw_error) :-
-    catch(omdb_fetch('Released'=_Value, [title="Casino Royale",year="200346"]),
-      Error,
-      Error=error(
-      existence_error(
-        key,
-        'Released',
-        _{'Error':"Movie not found!", 'Response':"False"}), _)
-    ).
+	catch(omdb_fetch('Released'=_Value, [title="Casino Royale",year="200346"]),
+	  Error,
+	  Error=error(
+	  existence_error(
+		key,
+		'Released',
+		_{'Error':"Movie not found!", 'Response':"False"}), _)
+	).
 
 test(search_title) :-
-    aggregate_all(count,
-      omdb_search_results(
-        'Title'=_Value,
-        [title="The Road to Casino Royale"],
-        _NumResults),
-      1
-    ).
+	aggregate_all(
+		count,
+		omdb_search_results(
+			'Title'=_Value,
+			[title="The Road to Casino Royale"],
+			_NumResults
+		),
+		1
+	).
 
 :- end_tests(pl_omdb).
